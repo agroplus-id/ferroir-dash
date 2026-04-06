610b230b2b9e5c4de69618bca936353239e80dd6
khaelano <khaelanoabroor@gmail.com>
2026-04-06 23:12:06 +0700

    change branding to fermego (change title)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Ferroir Dashboard",
+  title: "Fermego Dashboard",
   description: "Coffee Fermentation Monitoring System",
 };
 
